210baab2966f23eb5711823879b2edd3a74c8985
staryypes <88688038+staryypes@users.noreply.github.com>
2021-08-17 00:10:50 +0300

    fix_bug

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 $(document).ready(function () {
-    var new_block=new Array,
-    limit = 10;
+    var new_block,
+    quan=0,
 
+    hidden_blocks = new Array,
+    default_link = "https://icon-library.com/images/upload-icon/upload-icon-10.jpg", 
+    limit = 9;
+ 
+    $('#img').attr('src', default_link);
 
     $('#load_img').on('change', function () {
         if (this.files[0]) {
@@ -9,11 +14,11 @@ $(document).ready(function () {
             fr.addEventListener("load", function () {
                 $('#img').attr("src", fr.result);
                 $('#img').removeClass('error');
+                $('#error_img').remove();
             }, false);
             fr.readAsDataURL(this.files[0]);
         }
     });
-
 
     $('#title, #text').keyup(function () {
         if (!this.value.match(/^[\u0400-\u04ff]+$/)) {
@@ -23,6 +28,9 @@ $(document).ready(function () {
     });
 
 
+
+
+       
     $("#create_block").validate({
         rules: {
             title: {
@@ -52,60 +60,73 @@ $(document).ready(function () {
 
 
 
+
     $('#create').on('click', function () {
-        if ($('#img').attr('src') == 'https://icon-library.com/images/upload-icon/upload-icon-10.jpg') {
+        if ($('#img').attr('src') == default_link && !$('#error_img').length) {
             $('#img').addClass('error');
-        } else {
-            if ($("#create_block").valid()) {
-                var img = $('#img').attr('src'),
-                    title = $('#title').val(),
-                    link = $('#link').val(),
-                    text = $('#text').val();
-
-                new_block.push('<div class="blocks__block">' +
-                    '<a href="" class="blocks__block__link"' + link + '">' +
-                    '<div class="blocks__block__wrap">' +
-                    '<div class="blocks__block__img">' +
-                    '<img src="' + img + '">' +
-                    '</div>' +
-                    '<div class="blocks__block__title">' +
-                    '<p>' + title + '</p>' +
-                    '</div>' +
-                    '<div class="blocks__block__text">' +
-                    '<p>' + text + '</p>' +
-                    '</div>' +
-                    '</div>' +
-                    '</a>' +
-                    '</div>');
-
-
+            $('.main__form__errors').append('<label id="error_img" class="error_img" >Загрузите файл. </label>');
+        }
+        if ($("#create_block").valid() && $('#img').attr('src') != default_link) {
+            var img = $('#img').attr('src'),
+                title = $('#title').val(),
+                link = $('#link').val(),
+                text = $('#text').val();
                 
-                addBlock();
+            $('#error_img').remove();
                 
-            } 
-        }  
+
+            new_block = $('<div class="blocks__block">' +
+            '<a href="'+ link +'" class="blocks__block__link"">' +
+            '<div class="blocks__block__wrap">' +
+            '<div class="blocks__block__img">' +
+            '<img src="' + img + '">' +
+            '</div>' +
+            '<div class="blocks__block__title">' +
+            '<p>' + title + '</p>' +
+            '</div>' +
+            '<div class="blocks__block__text">' +
+            '<p>' + text + '</p>' +
+            '</div>' +
+            '</div>' +
+            '</a>' +
+            '</div>');
+
+            quan++;
+            addBlock();
+            $('#create_block').trigger("reset");
+            $('#img').attr('src', default_link);
+        } 
         
     });
 
     function addBlock() {
-        if (new_block.length < limit){
-            $('.blocks__block').each(function(){
-                $(this).remove();
-            });
-            $.each( new_block, function( key, value ) {
-                $('.blocks').append(new_block[key]);
-            });
+        if (quan <= limit){
+            $('.preloader').addClass('preloader_active');
+            $('.blocks').append(new_block);
+            $('.preloader').removeClass('preloader_active');
         } else {
+            hidden_blocks.push(new_block);
             $('.show_more').addClass('show');
         }
     }
 
     $('.loadMore').on('click', function(e){
         e.preventDefault();
-        limit += limit-1;
-        addBlock();
+        var x;
+        $('.preloader').addClass('preloader_active');
+        for(x = 0;hidden_blocks.length;x++){
+            if(x<limit){
+            $('.blocks').append(hidden_blocks[0]);
+            hidden_blocks.splice(0, 1);
+            } else {
+                $('.preloader').removeClass('preloader_active');
+                return
+            }
+        }
+        $('.preloader').removeClass('preloader_active');
+        quan = x;
         $('.show_more').removeClass('show');
-
+        
     });
 
 });
